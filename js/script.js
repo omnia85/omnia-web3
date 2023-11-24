ac1e2fd846d764e86a109e712cc99261133f5c8a
omnia85 <omniamostafaramadan@gmail.com>
2023-11-24 10:13:11 +0200

    create project

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// prompt( "do you want to visit Egypt?")
+
+// const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+// const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+// const myModal = document.getElementById('myModal')
+// const myInput = document.getElementById('myInput')
+
+// myModal.addEventListener('shown.bs.modal', () => {
+//   myInput.focus()
+// }) 
+// const myModal = document.getElementById('myModal')
+// const myInput = document.getElementById('myInput')
+
+// myModal.addEventListener('shown.bs.modal', () => {
+//   myInput.focus()
+// })
